461fc2b9f74b3a205ff7029c231764e6ef7548d9
Nathan Nguyen <nathannguyen1058@gmail.com>
2023-03-26 19:19:40 -0700

    finished api routes

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("./config/connection");
+const routes = require("./routes");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+mongoose.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+  });
+});
